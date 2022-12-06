4f301e72d5d3083a518f0c0edd2c3acef22a6737
dangertater <106702944+dangertater@users.noreply.github.com>
2022-12-05 18:40:52 -0700

    solutionWord is being generated correctly and readded all the right thing

---
+++ src/ButtonSolution.js
@@ -0,0 +1,42 @@
+import React from "react"
+import _ from "lodash"
+
+let wordGenerator = (num) => {
+	console.log(num, _.range(0, num))
+	return _.range(0, num)
+		.map(() => {
+			return 97 + Math.floor(Math.random() * 26)
+		})
+		.map((num) => {
+			return convertNumToLetter(num)
+		})
+}
+
+let convertNumToLetter = (num) => {
+	return String.fromCharCode(num)
+}
+
+let Button = (props) => {
+	return (
+		<>
+			<button
+				onClick={() => {
+					props.onClick(wordGenerator(props.num))
+				}}
+			>
+				{props.num}
+			</button>
+		</>
+	)
+}
+
+export default function ButtonSolution(props) {
+	const buttonRange = _.range(4, 10)
+	return (
+		<div>
+			{buttonRange.map((num) => {
+				return <Button {...props} key={num} num={num}></Button>
+			})}
+		</div>
+	)
+}
