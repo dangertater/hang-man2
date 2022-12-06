ca1c75bf4b1169e2a84a52c66daf209e4aa387a8
dangertater <106702944+dangertater@users.noreply.github.com>
2022-12-05 17:41:02 -0700

    added all lodash

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react"
-
+import React, { useState } from "react"
+import ButtonSolutionLength from "./ButtonSolutionLength"
 function App() {
-	return <></>
+	let [solutionWord, setSolutionWord] = useState(0)
+
+	return (
+		<>
+			{/* will have several buttons that all 'onClick' to setState of solution word */}
+			<div className="setSolutionWordLengthDiv">
+				<ButtonSolutionLength
+					solutionWord={solutionWord}
+					setSolutionWord={setSolutionWord}
+				></ButtonSolutionLength>
+			</div>
+			{/* will have a function that decides how many letters the solution word will have */}
+			<div className="solutionWordDiv">
+				<IndySolutionLetter />
+			</div>
+			<div className="guessedLettersDiv">Guessed Letters will go here</div>
+			<div className="alphabetButtonDiv"></div>
+		</>
+	)
 }
 
 export default App
