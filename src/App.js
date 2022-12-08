5a5c0a3fd1d8c6eb44aa9013cadcc9d2025598f6
dangertater <106702944+dangertater@users.noreply.github.com>
2022-12-07 20:21:21 -0700

    progress, added lodash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
 import React, { useState } from "react"
 import ButtonSolution from "./ButtonSolution"
 import _ from "lodash"
+import GuessButton from "./GuessButton"
 
 function App() {
 	let [solutionWord, setSolutionWord] = useState([])
+	let [guessedLetters, setGuessedLetters] = useState([])
+	let [notGuessedLetters, setNotGuessedLetters] = useState([
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"o",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"u",
+		"v",
+		"w",
+		"x",
+		"y",
+		"z",
+	])
+	// todo i need to figure otu the callback syntax to get the guessed letter out of the the notGuessedLettersState
+	let userGuessed = (guessedLetter) => {
+		setGuessedLetters([...guessedLetters, guessedLetter])
+		setNotGuessedLetters((guessedLetter) => {
+			return notGuessedLetters_.reject(() => {})
+		})
+		//q4e two questions why is my setTimeout not working and then
+		// ----why is it console logging after the render, when its clearly below the setState a few lines up
+		setTimeout(console.log("guessedLetters", guessedLetters), 2000)
+
+		console.log("notGuessedLetters", notGuessedLetters)
+	}
+	let handleClickGuessButton = (guessedLetter) => {
+		console.log("guessed letter pressed", guessedLetter)
+		userGuessed(guessedLetter)
+	}
 
 	return (
 		<>
@@ -16,12 +62,14 @@ function App() {
 						setSolutionWord(arrayOfLetters)
 					}}
 				></ButtonSolution>
-				<>the letters are {solutionWord.join("-")}</>
+				<>the solution letters are {solutionWord.join("-")}</>
 			</div>
 			<div className="guessedLettersDiv">Guessed Letters will go here</div>
 			<div className="alphabetButtonDiv">
-				<GuessButton />
+				<GuessButton handleClickGuessButton={handleClickGuessButton} />
 			</div>
+			<div>guessedLetters {guessedLetters}</div>
+			<div>notGuessedLetters {notGuessedLetters}</div>
 		</>
 	)
 }
--- src/GuessButton.js
+++ src/GuessButton.js
@@ -1,5 +1,46 @@
 import React from "react"
 
-export default function () {
-	return <div></div>
+export default function GuessButton(props) {
+	let alphabetArray = [
+		"a",
+		"b",
+		"c",
+		"d",
+		"e",
+		"f",
+		"g",
+		"h",
+		"i",
+		"j",
+		"k",
+		"l",
+		"m",
+		"n",
+		"o",
+		"p",
+		"q",
+		"r",
+		"s",
+		"t",
+		"u",
+		"v",
+		"w",
+		"x",
+		"y",
+		"z",
+	]
+	let Button = (indyLetter) => {
+		return (
+			<button
+				onClick={(e) => {
+					props.handleClickGuessButton(indyLetter.mappedLetter)
+				}}
+			>
+				{indyLetter.mappedLetter}
+			</button>
+		)
+	}
+	return alphabetArray.map((mappedLetter) => {
+		return <Button key={mappedLetter} mappedLetter={mappedLetter}></Button>
+	})
 }
